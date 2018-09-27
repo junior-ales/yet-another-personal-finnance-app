f11bae9667bd6d9467df9e09e3f06211af25e362
Junior Ales <edilson.ales.jr@gmail.com>
2018-09-28 00:59:04 +0100

    add formik and create the first inputs in tp new

--- src/tracking-period/tracking-period-new/TrackingPeriodNew.tsx
+++ src/tracking-period/tracking-period-new/TrackingPeriodNew.tsx
@@ -1,27 +1,65 @@
+import { Field, FieldProps, Form, Formik, FormikActions } from 'formik';
 import * as React from 'react';
+import * as uuid from 'uuid';
 
 import { TrackingPeriodNewProps } from '.';
 import { TrackingPeriod } from '../../shared/store';
 
+const newTrackingPeriod = (): TrackingPeriod => ({
+  endDate: new Date(),
+  id: uuid(),
+  initialBudget: 0,
+  plannedSavings: 0,
+  startDate: new Date()
+});
+
 export class TrackingPeriodNew extends React.Component<TrackingPeriodNewProps> {
-  public handleOnSave = (trackingPeriod: TrackingPeriod) => (): void => {
+  public handleOnSave = (
+    trackingPeriod: TrackingPeriod,
+    actions: FormikActions<TrackingPeriod>
+  ): void => {
     const { onSaveTrackingPeriod, history } = this.props;
     onSaveTrackingPeriod(trackingPeriod, history);
+    actions.setSubmitting(false);
   };
 
   public render() {
-    const { trackingPeriod } = this.props;
+    const trackingPeriod = this.props.trackingPeriod || newTrackingPeriod();
 
     return (
       <section>
-        {trackingPeriod && (
-          <section>
-            Id: <span>{trackingPeriod.id}</span>
-            <button type="button" onClick={this.handleOnSave(trackingPeriod)}>
-              Save
-            </button>
-          </section>
-        )}
+        <header>New Tracking Period</header>
+        <Formik initialValues={trackingPeriod} onSubmit={this.handleOnSave}>
+          {() => (
+            <Form>
+              <Field name="initialBudget">
+                {(props: FieldProps<TrackingPeriod>) => (
+                  <div>
+                    <label htmlFor={props.field.name}>Initial Budget</label>
+                    <input
+                      type="number"
+                      id={props.field.name}
+                      {...props.field}
+                    />
+                  </div>
+                )}
+              </Field>
+              <Field name="plannedSavings">
+                {(props: FieldProps<TrackingPeriod>) => (
+                  <div>
+                    <label htmlFor={props.field.name}>Planned Savings</label>
+                    <input
+                      type="number"
+                      id={props.field.name}
+                      {...props.field}
+                    />
+                  </div>
+                )}
+              </Field>
+              <button type="submit">Save</button>
+            </Form>
+          )}
+        </Formik>
       </section>
     );
   }
