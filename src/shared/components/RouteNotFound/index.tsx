98f579db83fe706316ec9a2f4d2a8c88b0376c0c
Junior Ales <edilson.ales.jr@gmail.com>
2018-09-30 19:12:27 +0100

    styling route not found comp

--- src/shared/components/RouteNotFound/index.tsx
+++ src/shared/components/RouteNotFound/index.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+import './routeNotFound.css';
+
 export const RouteNotFound: React.SFC<{}> = () => (
-  <section>
+  <section className="RouteNotFound">
     <h1>Ooops, pagina nao encontrada</h1>
     <p>
       Voltar para <Link to="/">pagina inicial</Link>.
