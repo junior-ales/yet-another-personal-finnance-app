13829073653f4fda0a263362e8f181027e4af86a
Junior Ales <edilson.ales.jr@gmail.com>
2018-10-06 22:26:00 +0100

    remove unused file

--- src/shared/store/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.initialState = {
-    trackingPeriods: { allId: [], byId: {}, selected: null },
-    transactions: { allId: [], byId: {}, byTrackingPeriod: {}, selected: null }
-    // user: { id: 'user1' }
-};
